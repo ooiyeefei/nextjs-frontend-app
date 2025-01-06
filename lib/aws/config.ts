627044c77dfb4835e1584b21f86b2b7eec31c252
Ubuntu <ubuntu@ip-10-0-2-212.us-west-2.compute.internal>
2025-01-06 16:06:06 +0000

    update aws config

--- lib/aws/config.ts
+++ lib/aws/config.ts
@@ -1,19 +1,18 @@
 import AWS from 'aws-sdk'
 
-export const awsConfig = {
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
-  }
-}
+const credentials = new AWS.Credentials({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? ''
+})
 
 export const ses = new AWS.SES({
   apiVersion: '2010-12-01',
-  ...awsConfig
+  region: process.env.AWS_REGION ?? 'ap-southeast-1',
+  credentials
 })
 
 export const sns = new AWS.SNS({
   apiVersion: '2010-03-31',
-  ...awsConfig
+  region: process.env.AWS_REGION ?? 'ap-southeast-1',
+  credentials
 })
